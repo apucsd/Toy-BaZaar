3af8dda6f5d4f1e4e1fbe2bce9597cc8cfee0b48
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-05-17 23:32:31 +0600

    added a footer

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/pages/shared/Navbar";
+import Footer from "../components/pages/shared/Footer";
 
 const Main = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
