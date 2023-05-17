9de80146e89032ead2a9b4d8dd974363d944b8b8
Apu Sutra Dhar <apusutradhar77@gmail.com>
2023-05-17 21:53:39 +0600

    set up basic routes

---
+++ src/components/pages/error-page/Error.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = () => {
+  return <div>eoor page</div>;
+};
+
+export default Error;
---
+++ src/routes/Route.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Error from "../components/pages/error-page/Error";
+import Home from "../components/pages/home/Home";
+import Login from "../components/pages/authentications/Login";
+import Register from "../components/pages/authentications/Register";
+import AllToys from "../components/pages/all-toys/AllToys";
+import MyToys from "../components/pages/my-toys/MyToys";
+import AddToy from "../components/pages/AddToys/AddToy";
+import Blog from "../components/pages/blogs/Blog";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/all-toys",
+        element: <AllToys></AllToys>,
+      },
+      {
+        path: "/my-toys",
+        element: <MyToys></MyToys>,
+      },
+      {
+        path: "/add-toy",
+        element: <AddToy></AddToy>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+export default router;
